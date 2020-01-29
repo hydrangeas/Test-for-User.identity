ccfddf9a415736e82ca6a8ece737e249d2096bc9
Akira Otaka <hydrangeas.project@gmail.com>
2020-01-29 22:23:21 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication2.Pages
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+        }
+
+        public string UserName = "";
+
+        public void OnGet()
+        {
+            UserName = User.Identity.Name;
+        }
+    }
+}
---
+++ WebApplication2Test.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+using WebApplication2.Pages;
+
+namespace WebApplication2Test.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ÉÜÅ[ÉUÅ[ñºÇéÊìæÇ≈Ç´ÇÈ()
+        {
+            var httpContext = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal(new GenericIdentity("Akira"))
+            };
+
+            var actionContext = new ActionContext(
+                httpContext,
+                new RouteData(),
+                new PageActionDescriptor(),
+                new ModelStateDictionary());
+
+            var indexModel = new IndexModel() { PageContext = new PageContext(actionContext) };
+            indexModel.OnGet();
+
+            indexModel.UserName.Is("Akira");
+        }
+    }
+}
